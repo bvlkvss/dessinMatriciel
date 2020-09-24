26416c7ddc3fdce15b00f8ca9e91c798c0116004
yahamh <51295289+yahamh@users.noreply.github.com>
2020-09-24 00:22:21 -0400

    adding ui

--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -1,7 +1,25 @@
 import { Component } from '@angular/core';
+import { ToolsManagerService } from '@app/services/toolsManger/tools-manager.service';
+
+
 @Component({
     selector: 'app-sidebar',
     templateUrl: './sidebar.component.html',
     styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent {}
+
+
+export class SidebarComponent {
+
+    constructor(private tools: ToolsManagerService){}
+  
+    
+    changeTools(id : number){
+        this.tools.setTools(id);
+        //id.setFocus();
+        
+    }
+
+      
+}
+
