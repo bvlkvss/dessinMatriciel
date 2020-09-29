1325243b9528bc9c906020710f97224f8ed75ad8
Nassim Lebdiri <nassimlbd@gmail.com>
2020-09-29 12:09:15 -0400

    buttons added

--- client/src/app/services/tools/eraser/eraser-service.ts
+++ client/src/app/services/tools/eraser/eraser-service.ts
@@ -21,6 +21,8 @@ export class EraserService extends Tool {
 
     constructor(drawingService: DrawingService) {
         super(drawingService);
+        this.toolAttributes = ['lineWidth'];
+
         this.clearPath();
     }
 
