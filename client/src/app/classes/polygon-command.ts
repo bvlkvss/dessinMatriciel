217ff9af08d07f3c3b98fb8a522115bcb40a262e
anass laaz <anasslaaz@localhost.localdomain>
2020-11-05 13:42:14 -0500

    saving number side for each polygon command

--- client/src/app/classes/polygon-command.ts
+++ client/src/app/classes/polygon-command.ts
@@ -10,6 +10,7 @@ export class PolygonCommand extends Command {
     private primaryColor: string;
     private secondaryColor: string;
     private opacity: number;
+    private numberside: number;
     isResize: boolean = false;
 
     constructor(startPos: Vec2, endPos: Vec2, style: number, protected tool: PolygonService, protected drawingService: DrawingService) {
@@ -20,9 +21,11 @@ export class PolygonCommand extends Command {
         this.primaryColor = this.tool.primaryColor;
         this.secondaryColor = this.tool.secondaryColor;
         this.opacity = this.tool.opacity;
+        this.numberside = this.tool.numberSides;
     }
     execute(): void {
         this.tool.polygonStyle = this.style;
+        this.tool.numberSides = this.numberside;
         this.tool.primaryColor = this.primaryColor;
         this.tool.secondaryColor = this.secondaryColor;
         this.tool.opacity = this.opacity;
