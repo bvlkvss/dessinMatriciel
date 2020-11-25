a8107d34633dba37c800daebda05dfc74939d99b
Haroun Khalfi <haroun.khalfi@polymtl.ca>
2020-11-24 20:27:12 -0500

    generated spray paint service

---
+++ client/src/app/services/tools/spray-paint/spray-paint.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { SprayPaintService } from './spray-paint.service';
+
+describe('SprayPaintService', () => {
+  let service: SprayPaintService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(SprayPaintService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
