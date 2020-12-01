600988f9f8efd840182520f97871493eb6f2c397
yahamh <51295289+yahamh@users.noreply.github.com>
2020-11-30 22:44:28 -0500

    tool activation onclick

--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -121,6 +121,7 @@ export class SidebarComponent implements OnChanges {
     }
 
     changeTools(name: string): void {
+        console.log('ok');
         this.drawingService.restoreCanvasState();
         if (this.tools.currentTool instanceof TextService && name !== 'text') (this.tools.currentTool as TextService).drawConfirmedText(true);
         this.tools.setTools(name);
@@ -132,6 +133,7 @@ export class SidebarComponent implements OnChanges {
         for (let i = 0; i < numberOfTools; i++) {
             document.getElementsByTagName('a')[i].classList.remove('active');
         }
+        document.getElementById(name)?.setAttribute('class', 'active');
         document.getElementById(this.currentToolName)?.setAttribute('class', 'active');
     }
 
