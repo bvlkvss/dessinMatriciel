a6044df36eb12080a1d2cf5aecffe2e7848e1d87
Nassim Lebdiri <nassimlbd@gmail.com>
2020-11-30 22:59:58 -0500

    tools manager fixed

--- client/src/app/services/tools-manager/tools-manager.service.ts
+++ client/src/app/services/tools-manager/tools-manager.service.ts
@@ -3,13 +3,17 @@ import { Tool } from '@app/classes/tool';
 import { BrushService } from '@app/services/tools/brush/brush.service';
 import { EllipseService } from '@app/services/tools/ellipse/ellipse.service';
 import { EraserService } from '@app/services/tools/eraser/eraser-service';
+import { GridService } from '@app/services/tools/grid/grid.service';
 import { LineService } from '@app/services/tools/line/line.service';
+import { MagicWandService } from '@app/services/tools/magic-wand/magic-wand.service';
 import { PaintBucketService } from '@app/services/tools/paint-bucket/paint-bucket.service';
 import { PencilService } from '@app/services/tools/pencil/pencil-service';
 import { PipetteService } from '@app/services/tools/pipette/pipette.service';
+import { PlumeService } from '@app/services/tools/plume/plume.service';
 import { PolygonService } from '@app/services/tools/polygon/polygon.service';
 import { RectangleService } from '@app/services/tools/rectangle/rectangle.service';
 import { SelectionService } from '@app/services/tools/selection/selection.service';
+import { SprayPaintService } from '@app/services/tools/spray-paint/spray-paint.service';
 import { TextService } from '@app/services/tools/text/text.service';
 
 @Injectable({
@@ -31,6 +35,10 @@ export class ToolsManagerService {
         polygonService: PolygonService,
         pipetteService: PipetteService,
         textService: TextService,
+        sprayPaintService: SprayPaintService,
+        plumeService: PlumeService,
+        gridService: GridService,
+        magicWandService: MagicWandService,
     ) {
         this.tools = new Map<string, Tool>([
             ['pencil', pencilService],
@@ -45,8 +53,20 @@ export class ToolsManagerService {
             ['polygon', polygonService],
             ['pipette', pipetteService],
             ['text', textService],
+            ['plume', plumeService],
+            ['aerosol', sprayPaintService],
+            ['grid', gridService],
+            ['magic-wand', magicWandService],
         ]);
         this.currentTool = this.tools.get('pencil') as Tool;
+    }
+
+    getByValue(searchValue: Tool): string {
+        let toolname = '';
+        for (const [key, value] of this.tools.entries()) {
+            if (value === searchValue) toolname = key;
+        }
+        return toolname;
     }
 
     setTools(name: string): void {
@@ -103,5 +123,20 @@ export class ToolsManagerService {
     setJunctionState(isChecked: boolean): void {
         const line = this.currentTool as LineService;
         line.setJunctionState(isChecked);
+    }
+
+    setDropletsWidth(value: number): void {
+        const spray = this.currentTool as SprayPaintService;
+        spray.setDropletsWidth(value);
+    }
+
+    setFrequency(value: number): void {
+        const spray = this.currentTool as SprayPaintService;
+        spray.setfrequency(value);
+    }
+
+    setRadius(value: number): void {
+        const spray = this.currentTool as SprayPaintService;
+        spray.setRadius(value);
     }
 }
